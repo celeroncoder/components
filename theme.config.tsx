759a9b3f9bf00dd0b6a7aec412a3a96a6674b152
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-11-26 01:59:59 +0530

    added custom dynamic header

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,36 @@
 import type { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://components.celeroncoder.com" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:title"
+          content={
+            `${frontMatter.title} | celeroncoder's components` ||
+            "celeroncoder's components"
+          }
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Collection of shadcn-ui/cli compatible components dump thing"
+          }
+        />
+        <title>{frontMatter.title || "celeroncoder's components"}</title>
+      </>
+    );
+  },
   project: {
     link: "https://github.com/celeroncoder/components/",
   },
