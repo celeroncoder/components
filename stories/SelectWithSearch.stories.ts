0d4a5a4aaedd7ceef5d0d1ce5ce1af6dcfd1db8f
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-11-27 02:05:31 +0530

    added storybook preview command

--- stories/SelectWithSearch.stories.ts
+++ stories/SelectWithSearch.stories.ts
@@ -1,11 +1,26 @@
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { SelectWithSearchDemo } from "./SelectWithSearchDemo";
+import { SelectWithSearchDemo } from "@/registry/default/example/select-with-search-demo";
 
 const meta: Meta<typeof SelectWithSearchDemo> = {
   component: SelectWithSearchDemo,
+  parameters: {
+    viewport: {
+      viewports: MINIMAL_VIEWPORTS,
+      defaultViewport: "desktop",
+    },
+  },
 };
 
 export default meta;
 
-export const Primary: StoryObj<typeof SelectWithSearchDemo> = {};
+export const Desktop: StoryObj<typeof SelectWithSearchDemo> = {};
+export const Mobile: StoryObj<typeof SelectWithSearchDemo> = {
+  parameters: {
+    viewport: {
+      viewports: MINIMAL_VIEWPORTS,
+      defaultViewport: "mobile1",
+    },
+  },
+};
